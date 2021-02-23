67bf2d1b2185c94ebc944134e96aed15f5c3a026
nguyenhung <hunghung0309@gmail.com>
2021-02-23 11:49:10 +0700

    start signup

--- DEV/open techiz/React Native/RN-track-server/src/index.js
+++ DEV/open techiz/React Native/RN-track-server/src/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
 
 const app = express();
+
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:admin@cluster0.ll9p8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    userNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo!....');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error("Error...", err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
