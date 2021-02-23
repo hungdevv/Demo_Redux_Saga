f2e4b22512896d8165fb9cf25f8c7886a7f14188
nguyenhung <hunghung0309@gmail.com>
2021-02-23 14:37:23 +0700

    185. JWT

--- DEV/open techiz/React Native/RN-track-server/src/index.js
+++ DEV/open techiz/React Native/RN-track-server/src/index.js
@@ -2,7 +2,8 @@ require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -23,8 +24,8 @@ mongoose.connection.on('error', (err) => {
     console.error("Error...", err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
