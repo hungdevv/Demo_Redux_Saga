e6ce65031b65dc71a4fd43e17cb2535ddcd2029d
nguyenhung <hunghung0309@gmail.com>
2021-02-24 11:32:25 +0700

    navigation

--- DEV/open techiz/React Native/track-server/track-server/tracks/src/context/AuthContext.js
+++ DEV/open techiz/React Native/track-server/track-server/tracks/src/context/AuthContext.js
@@ -1,23 +1,32 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { AsyncStorage } from 'react-native';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload }
+    case 'signup':
+      return {errorMessage: '', token: action.payload};
     default:
       return state;
   }
 };
 
-const signup = dispatch => {
-  return async ({ email, password }) => {
+
+
+const signup = dispatch => async ({ email, password }) => {
     try {
       const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({type: 'signup', payload: response.data.token});
+
+      navigate('TrackList');
     } catch (err) {
-      console.log(err.response.data);
+      dispatch({type: 'add_error', payload: 'Something went wrong with sign up'});
     }
   };
-};
 
 const signin = dispatch => {
   return ({ email, password }) => {
@@ -36,5 +45,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { token: null, errorMessage: '' }
 );
--- DEV/open techiz/React Native/track-server/track-server/tracks/src/screens/SignupScreen.js
+++ DEV/open techiz/React Native/track-server/track-server/tracks/src/screens/SignupScreen.js
@@ -9,6 +9,9 @@ const SignupScreen = ({ navigation }) => {
     const {state, signup} = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    // console.log(state);
+
     return(
         <View style={styles.container}>  
             <Spacer>
@@ -32,6 +35,7 @@ const SignupScreen = ({ navigation }) => {
                 autoCapitalize="none"
                 autoCorrect={false} />
             </Spacer>
+            {state.errorMessage ? <Text style={styles.errorMessage}> {state.errorMessage}</Text> : null}
             <Spacer>
                 <Button 
                 title = "Sign Up"
@@ -62,6 +66,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center', 
         marginBottom: 200
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginBottom: 15
     }
 });
 
