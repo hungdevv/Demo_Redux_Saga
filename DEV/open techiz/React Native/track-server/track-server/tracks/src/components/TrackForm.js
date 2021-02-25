2de7c707d3b8b85f70f49f62d5b7871a2e651773
nguyenhung <hunghung0309@gmail.com>
2021-02-25 11:53:42 +0700

    250

--- DEV/open techiz/React Native/track-server/track-server/tracks/src/components/TrackForm.js
+++ DEV/open techiz/React Native/track-server/track-server/tracks/src/components/TrackForm.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
+import { Context as LocationContext} from '../context/LocationContext';
 
 const TrackForm = () => {
-    return <>
+    const {state: { name, recording, locations }, 
+    startRecording, 
+    stopRecording, 
+    changeName} = useContext(LocationContext);
+    
+    console.log(locations);
+
+    return (<>
         <Spacer>
-            <Input placeholder="Enter name..."/>
-            < Button title="Start Recording"/>
+            <Input 
+            value={name}
+            placeholder="Enter name..."
+            onChangeText={changeName}/>
+            {recording 
+                ? <Button title="Stop Recording" onPress={stopRecording}/>
+                : <Button onPress={startRecording} title="Start Recording"/>
+            }
         </Spacer>
         </>
+    )
 
 };
 
